afdbd67accf1b937d0f6821334f7fa94ecc31c4b
AlexVoron123 <aleksei.vorontsov052@gmail.com>
2025-04-14 11:05:22 +0300

    task

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,26 @@
 export function romanToInteger(str) {
     let result = 0;
-    /*
-    Напишите код, преобразующий число из римской записи в арабскую.
-    На вход вы получите строку, ответом должно быть число.
-     */
+    const romanNumerals = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    };
+    for (let i = 0; i < str.length; i++) {
+        const current = romanNumerals[str[i]];
+        const next = romanNumerals[str[i + 1]];
+
+        if (next && current < next) {
+            result -= current;
+        } else {
+            result += current;
+        }
+    }
+
+    return result;
+
     return result;
 }
